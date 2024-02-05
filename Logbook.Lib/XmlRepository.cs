ea69820c197de07a65729245de5299f272156233
David Moser <damoser@schueler.hakzell.at>
2024-02-05 12:18:19 +0100

    add create, delete, save method

--- Logbook.Lib/XmlRepository.cs
+++ Logbook.Lib/XmlRepository.cs
@@ -9,10 +9,13 @@ namespace Logbook.Lib
 {
     public class XmlRepository : IRepository
     {
-        private XElement _rootElement;
+        private readonly XElement _rootElement;
+
+        private readonly string _path;
 
         public XmlRepository(string path)
         {
+            _path = path;
             if(File.Exists(path))
             {
                 _rootElement = XElement.Load(path);
@@ -25,12 +28,48 @@ namespace Logbook.Lib
 
         public bool Add(Entry entry)
         {
-            throw new NotImplementedException();
+            XElement node = new("entry");
+
+            var idAttrib = new XAttribute("id", entry.Id.ToString());
+            node.Add(idAttrib);
+
+            var startAttrib = new XAttribute("start", entry.Start.ToString());
+            node.Add(startAttrib);
+
+            var endAttrib = new XAttribute("end", entry.End.ToString());
+            node.Add(endAttrib);
+
+            var startKmAttrib = new XAttribute("startkm", entry.StartKM.ToString());
+            node.Add(startKmAttrib);
+
+            var endKmAttrib = new XAttribute("endkm", entry.EndKM.ToString());
+            node.Add(endKmAttrib);
+
+            var numberPlateAttrib = new XAttribute("numberplate", entry.NumberPlate.ToString());
+            node.Add(numberPlateAttrib);
+
+            var fromAttrib = new XAttribute("from", entry.From.ToString());
+            node.Add(fromAttrib);
+
+            var toAttrib = new XAttribute("to", entry.To.ToString());
+            node.Add(toAttrib);
+
+            node.Add(entry.Description.ToString());
+
+            _rootElement.Add(node);
+
+            return this.Save();
         }
 
         public bool Delete(Entry entry)
         {
-            throw new NotImplementedException();
+            var itemsDel = from e in _rootElement.Descendants("entry")
+                           where (string)e.Attribute("id") == entry.Id
+                           select e;
+
+            itemsDel.Remove();
+
+            return this.Save();
         }
 
         public List<Entry> GetAll()
@@ -47,7 +86,16 @@ namespace Logbook.Lib
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            try
+            {
+                _rootElement.Save(_path);
+                return true;
+            }
+            catch( Exception ex)
+            {
+                return false;
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
         }
 
         public bool Update(Entry entry)
