c978e5401d0482da7392e1b17bb340415fa266f2
David Moser <damoser@schueler.hakzell.at>
2024-02-08 08:03:28 +0100

    Add new entry to IRepository

--- Logbook.Lib/IRepository.cs
+++ Logbook.Lib/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,13 @@ namespace Logbook.Lib
         bool Delete(Entry entry);
 
         bool Update(Entry entry);
+
+        /// <summary>
+        /// Search for an entry
+        /// </summary>
+        /// <param name="id">the id to search for</param>
+        /// <returns>Entry or null</returns>
+        Entry? find(string id);
 
         bool Save();
 
