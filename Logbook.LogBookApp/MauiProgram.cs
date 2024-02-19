4521a6e5de389daeb145cceb79e7d7477bb3a4eb
David Moser <damoser@schueler.hakzell.at>
2024-02-19 12:00:04 +0100

    add path and repository to app

--- Logbook.LogBookApp/MauiProgram.cs
+++ Logbook.LogBookApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Logbook.LogBookApp.ViewModel;
+﻿using Logbook.Lib;
+using Logbook.LogBookApp.ViewModel;
 using Microsoft.Extensions.Logging;
 
 namespace Logbook.LogBookApp
@@ -18,6 +19,13 @@ namespace Logbook.LogBookApp
 
             builder.Services.AddSingleton<MainPage>();
             builder.Services.AddSingleton<MainViewModel>();
+
+            string path = FileSystem.Current.AppDataDirectory;
+            string filename = "data.xml";
+            string fullpath = System.IO.Path.Combine(path, filename);
+            System.Diagnostics.Debug.WriteLine(fullpath);
+
+            builder.Services.AddSingleton<IRepository>(new XmlRepository(fullpath));
 
 #if DEBUG
     		builder.Logging.AddDebug();
